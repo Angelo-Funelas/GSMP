b075b9c98288055bef71060dfad3963e053df8c9
git push <martinangelom.funelas.10@gmail.com>
2021-06-23 17:07:01 +0800

    added requestsent notif

--- smp/views.py
+++ smp/views.py
@@ -4,11 +4,14 @@ from .forms import *
 # Create your views here.
 
 def index(request):
+    requestsent = ""
     form = new_member(request.POST or None)
     if form.is_valid():
         form.save()
+        requestsent = True
     return render(request, "smp/index.html", {
         "members": Member.objects.filter(Approved=True),
-        "form": form
+        "form": form,
+        "requestsent": requestsent
     })
     
